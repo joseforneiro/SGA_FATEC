9a5587580dd495d6e17e5b588f122b446e937e7c
HelenAlevato <helen-alevato@hotmail.com>
2021-11-07 20:10:12 -0300

    Ajustes e Adição do Update de Evento

--- eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -2,19 +2,23 @@ package com.eventoapp.controllers;
 
 import javax.validation.Valid;
 
+import com.eventoapp.models.Convidado;
+import com.eventoapp.models.ConvidadosWrapper;
+import com.eventoapp.models.Evento;
+import com.eventoapp.repository.ConvidadoRepository;
+import com.eventoapp.repository.EventoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.eventoapp.models.Convidado;
-import com.eventoapp.models.Evento;
-import com.eventoapp.repository.ConvidadoRepository;
-import com.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
@@ -31,7 +35,7 @@ public class EventoController {
 	}
 	
 	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
-	public String form(@Valid Evento evento, BindingResult result, RedirectAttributes attributes) {
+	public String salvarEvento(@Valid Evento evento, BindingResult result, RedirectAttributes attributes) {
 		if(result.hasErrors()) {
 			attributes.addFlashAttribute("mensagem", "Verifique os campos!");
 			return "redirect:/cadastrarEvento";
@@ -44,7 +48,7 @@ public class EventoController {
 	// Método para retornar a lista de eventos:
 	
 	@RequestMapping("/eventos") // dentro das aspas ficam o que vai retornar. Vai retornar o evento
-	public ModelAndView listaEventos(){
+	public ModelAndView listarEventos(){
 		ModelAndView mv = new ModelAndView("index"); //index é a página que vai ser renderizada: index.html
 		Iterable<Evento> eventos = er.findAll(); // Busca toda a lista de eventos
 		mv.addObject("eventos", eventos); // O "eventos" dentro das aspas é a palavra eventos que está nessa parte do código do index.html (${eventos}): <div th:each="evento : ${eventos}">
@@ -58,10 +62,38 @@ public class EventoController {
 		mv.addObject("evento", evento); // O "evento" dentro das aspas é a palavra evento que está nessa parte do código do detalhesEvento.html (${evento}): <div th:each="evento : ${evento}">
 		System.out.println("evento" + evento);
 		
-		Iterable<Convidado> convidados = cr.findByEvento(evento); // Pega a lista de convidados através do código do evendo. O método está em ConvidadoRepository.java
-		mv.addObject("convidados", convidados); // Aqui enviamos a lista para a view
+		ConvidadosWrapper convidadosWrapper = new ConvidadosWrapper(cr.findByEvento(evento)); // Pega a lista de convidados através do código do evendo. O método está em ConvidadoRepository.java
+		mv.addObject("convidadosWrapper", convidadosWrapper); // Aqui enviamos a lista para a view
 		
 		return mv;
+	}
+	
+	@RequestMapping(value="/EditarEvento/{codigo}", method=RequestMethod.GET) 
+	public ModelAndView buscarEventoParaEditar(@PathVariable("codigo") long codigo) {
+		Evento evento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/editarDetalhesEvento"); 
+		mv.addObject("evento", evento);
+		System.out.println("evento" + evento);
+		
+		Iterable<Convidado> convidados = cr.findByEvento(evento); 
+		mv.addObject("convidados", convidados); 
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/EditarEvento/{codigo}", method=RequestMethod.POST) 
+	public String editarEvento(@PathVariable("codigo") long codigo, @Valid Evento evento) {
+//		ModelAndView mv = new ModelAndView("evento/editarDetalhesEvento");
+		er.save(evento);
+		
+		return "redirect:/eventos";
+	}
+	
+	@RequestMapping("/deletarEvento")
+	public String deletarEvento(long codigo){
+		Evento evento = er.findByCodigo(codigo);
+		er.delete(evento);
+		return "redirect:/eventos";
 	}
 	
 	@RequestMapping(value="/{codigo}", method=RequestMethod.POST) // vai retornar o código de cada evento e mostrar os detalhes dele
@@ -77,4 +109,32 @@ public class EventoController {
 		return "redirect:/{codigo}";
 	}
 	
+	@RequestMapping("/deletarConvidado")
+	public String deletarConvidado(String rg){
+		Convidado convidado = cr.findByRg(rg);
+		cr.delete(convidado);
+		
+		Evento evento = convidado.getEvento();
+		long codigoLong = evento.getCodigo();
+		String codigo = "" + codigoLong;
+		return "redirect:/" + codigo;
+		
+	}
+	
+	@PostMapping("/editarConvidado")
+	public String editarConvidado(String rg){
+		Convidado convidado = cr.findByRg(rg);
+		cr.save(convidado);
+		
+		Evento evento = convidado.getEvento();
+		long codigoLong = evento.getCodigo();
+		String codigo = "" + codigoLong;
+		return "redirect:/" + codigo;
+	}
+
+	@PostMapping("/editConvidados")
+	public String editarConvidados(@ModelAttribute("convidadosWrapper") ConvidadosWrapper convidadosWrapper, Model model){
+		System.out.println(convidadosWrapper.getConvidados());
+		return "redirect:/" + 1;
+	}
 }
--- eventoapp/src/main/java/com/eventoapp/repository/ConvidadoRepository.java
+++ eventoapp/src/main/java/com/eventoapp/repository/ConvidadoRepository.java
@@ -1,11 +1,12 @@
 package com.eventoapp.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.eventoapp.models.Convidado;
 import com.eventoapp.models.Evento;
+
+import org.springframework.data.repository.CrudRepository;
 
 public interface ConvidadoRepository extends CrudRepository<Convidado, String> {
 
 	Iterable<Convidado> findByEvento(Evento evento); // Método para buscar a lista de convidados pelo Evento
+	Convidado findByRg(String rg);
 }
